7042c34af59f1fb33c5cab076d52c265931f3257
sdioffi <y4d9p5rfnn@privaterelay.appleid.com>
2026-08-19 11:09:52 +0500

    Update script.js

--- medbazar_site/script.js
+++ medbazar_site/script.js
@@ -1,23 +1,11 @@
-// Инициализация слайдера (Полный клон логики Uzum Market)
-const swiper = new Swiper('.hero-swiper', {
-    // Бесконечная прокрутка
-    loop: true,
+let tg = window.Telegram.WebApp;
+tg.expand();
 
-    // Автоматическое перелистывание (даже если не наводить курсор)
-    autoplay: {
-        delay: 5000, // Перелистывает каждые 3.5 секунды
-        disableOnInteraction: false, // Не останавливать после того, как пользователь кликнул
-    },
+// Жестко включаем режим Телеграма
+document.body.classList.add('telegram-app');
 
-    // Стрелки перелистывания
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // Точки внизу баннера
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
+// Включаем нижнюю кнопку
+tg.MainButton.setText("ОФОРМИТЬ ЗАКАЗ");
+tg.MainButton.color = "#159447";
+tg.MainButton.textColor = "#ffffff";
+tg.MainButton.show();
